f552b0452b6ff72f020e344ba0328742db0268c4
Pawel <pmisiak@emarketer.com>
2021-09-15 16:10:46 -0400

    main first commit

---
+++ index.js
@@ -0,0 +1,32 @@
+const base = require('./lib/base.js');
+const next = require('./lib/next.js');
+const react = require('./lib/react.js');
+
+module.exports = {
+   extends: ['airbnb', 'airbnb/hooks', 'plugin:prettier/recommended'],
+   env: {
+      browser: true,
+      commonjs: true,
+      es2021: true,
+      node: true,
+      jest: true,
+   },
+   parserOptions: {
+      ecmaVersion: 2021,
+      sourceType: 'module',
+      ecmaFeatures: {
+         jsx: true,
+      },
+   },
+   plugins: ['simple-import-sort', 'import'],
+   rules: {
+      ...base,
+      ...next,
+      ...react,
+   },
+   settings: {
+      react: {
+         version: 'detect',
+      },
+   },
+};
